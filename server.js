744cdf7a2990098f5f6b5c6ddf5a1116f553fb6c
Ola Podsiadlik <alecs@op.pl>
2019-03-31 20:54:01 +0200

    Add google auth

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ app.get('/', function(req, res){
 });
 
 app.get('/logged', function(req, res){
-    res.render('logged', { user: googleProfile });
+    
+    res.render('logged', { user: String(googleProfile.displayName) });
 });
 //Passport routes
 app.get('/auth/google',
@@ -52,36 +53,3 @@ app.get('/auth/google/callback',
 
 app.listen(3000);
 
-// app.set('view engine', 'pug');
-// app.set('views','./views');
-// app.use('/store', function(req, res, next){
-//     console.log('Jestem pośrednikiem przy żądaniu do /store');
-//     next();
-// });
-
-// app.get('/', function (req, res) {
-//     res.send('Hello world!');
-// });
-
-// app.get('/store', function (req, res) {
-//     res.send('To jest sklep');
-// });
-
-// app.get('/auth/google', function(req, res){
-//     res.render('login');
-// });
-
-// app.get('/logged', function (req, res) {
-    
-//     res.render('logged', {  
-//         first_name: req.query.first_name,
-//         last_name: req.query.last_name 
-//     });  
-    
-    
-// });
-
-app.listen(3000);
-// app.use(function (req, res, next) {
-//     res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
-// });
